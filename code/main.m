aed5fd701bf215a65d7e556a8e3a9784c143408f
Sky020 <2397860h@student.gla.ac.uk>
2021-02-05 19:44:08 +0000

    udpate code again...

--- code/main.m
+++ code/main.m
@@ -10,6 +10,8 @@
 % n_1 - refractive index of core
 % n_2 - refractive index of cladding
 % n_3 - refractive index of surrounding medium
+% n_eff_1 - effective refractive index of core
+% n_eff_2 - effective refractive index of cladding
 
 % lambda - wavelength of light
 
@@ -22,6 +24,12 @@
 
 % FUNCTIONS ---------------------
 
+% core_lp_approx = (lambda_0: float, r_1: float, n_eff_1: float)
+% => [left: float, right: float] - linearly polarised approximation for core
+
+% coremode_n_eff(lambda_0: float, r_1: float)
+% => float - Effective refractive index for core
+
 % V = (lambda: float, n_1: float, n_2: float) => float - normalised frequency
 % J = (alpha: int, z: float) => float - Bessel function of the first kind
 % Y = (alpha: int, z: float) => float - Bessel function of the second kind
@@ -33,20 +41,20 @@
 % -------------------------------
 % GLOBAL VARIABLES
 % -------------------------------
-
-sumprod_upper = 1000; % As Bessel Functions use infinite summations, this defines the upper bound
+global step_size
+step_size = 0.000000000000001; % Bisection step size
+sumprod_upper = 10000; % As Bessel Functions use infinite summations, this defines the upper bound
 
 r_1 = 4.15E-6;
 r_2 = 62.5E-6;
 
-global SELLMEIER_COEFFICIENTS
-SELLMEIER_COEFFICIENTS = [0.7028554,0.4146307,0.8974540,0.0727723,0.1143085,9.896161];
+global SELLMEIER_COEFFICIENTS_CORE SELLMEIER_COEFFICIENTS_CLAD
+SELLMEIER_COEFFICIENTS_CORE = [0.7028554,0.4146307,0.8974540,0.0727723,0.1143085,9.896161];
+SELLMEIER_COEFFICIENTS_CLAD = [0.6961663,0.4079426,0.8974794,0.0684043,0.1162414,9.896161];
 
 % -------------------------------
 % BISECTION METHOD (NATIVE)
 % -------------------------------
-
-% n_1 = Sellmeier(lambda, SELLMEIER_COEFFICIENTS);
 
 % -------------------------------
 % TEST
@@ -54,47 +62,48 @@ SELLMEIER_COEFFICIENTS = [0.7028554,0.4146307,0.8974540,0.0727723,0.1143085,9.89
 temo = size(0,300);
 i = 1300:1601;
 for ii = i
-    temo(ii-1299) = coremode_function(ii*power(10,-9),r_1,r_2);
+    temo(ii-1299) = coremode_n_eff(ii*power(10,-9),r_1);
 end
 plot(i,temo)
 
-function [left_fun, right_fun] = core_formula_function(lambda0,r_1,r_2,neffc)
-    global SELLMEIER_COEFFICIENTS
-    n_1 = Sellmeier(lambda0, SELLMEIER_COEFFICIENTS);
-    n_2 = Sellmeier(lambda0, SELLMEIER_COEFFICIENTS);
-    b = (power(neffc,2) - power(n_2,2))/(power(n_1,2) - power(n_2,2));
-    v = V(lambda0, n_1, n_2, r_1);
-    temp_value = 1 - b;
-    sqr_temp1 = sqrt(temp_value);
+function [left, right] = core_lp_approx(lambda_0,r_1,n_eff_1)
+    global SELLMEIER_COEFFICIENTS_CORE SELLMEIER_COEFFICIENTS_CLAD
+    n_1 = Sellmeier(lambda_0, SELLMEIER_COEFFICIENTS_CORE);
+    n_2 = Sellmeier(lambda_0, SELLMEIER_COEFFICIENTS_CLAD);
+    b = (n_eff_1^2 - n_2^2)/(n_1^2 - n_2^2);
+    v = V(lambda_0, n_1, n_2, r_1);
+    
+    sqr_temp1 = sqrt(1-b);
     sqr_temp2 = sqrt(b);
     c = v * sqr_temp1;
     d = J(1, c);
     e = J(0, dc);
-    left_fun = c * d / e;
+    left = c * d / e;
     f = v *sqr_temp2;
     g = Y(1,f);
     h = Y(0,f);
-    right_fun = f * g/ h;
-    
+    right = f * g/ h;
 end
 
 
-function [n_eff_co_precise] = coremode_function(lambda0,r_1,r_2)
-    global SELLMEIER_COEFFICIENTS
-    n_1 = Sellmeier(lambda0, SELLMEIER_COEFFICIENTS);
-    n_2 = Sellmeier(lambda0, SELLMEIER_COEFFICIENTS);
-    n_eff_prev_core = n_2 + 0.000000000000001;
-    n_eff_post_core = n_1 - 0.000000000000001;
-    while n_eff_post_core - n_eff_prev_core > 0.000000001
-        n_eff_co_precise = (n_eff_prev_core + n_eff_post_core)/2;
-        left_right_funs = core_formula_function(lambda0,r_1,r_2,n_eff_co_precise);
-        if (left_right_funs(1) - left_right_funs(2) <= 0)
-            n_eff_prev_core = n_eff_co_precise;
-        elseif (left_right_funs(1) - left_right_funs(2) > 0)
-            n_eff_post_core = n_eff_co_precise;
+function [n_eff_1] = coremode_n_eff(lambda_0,r_1)
+    global SELLMEIER_COEFFICIENTS_CORE SELLMEIER_COEFFICIENTS_CLAD step_size;
+    n_1 = Sellmeier(lambda_0, SELLMEIER_COEFFICIENTS_CORE);
+    n_2 = Sellmeier(lambda_0, SELLMEIER_COEFFICIENTS_CLAD);
+    n_eff_1_prev = n_2 + step_size;
+    n_eff_1_post = n_1 - step_size;
+    while n_eff_1_post - n_eff_1_prev > 0.000000001
+        n_eff_1 = (n_eff_1_prev + n_eff_post_core)/2;
+        left_right = core_lp_approx(lambda_0,r_1,n_eff_1);
+        
+        % What happens when left_right(1) === left_right(2)...?
+        if (left_right(1) - left_right(2) < 0)
+            n_eff_1_prev = n_eff_1;
+        elseif (left_right(1) - left_right(2) > 0)
+            n_eff_1_post = n_eff_1;
         end
     end
-    n_eff_co_precise = (n_eff_prev_core + n_eff_post_core)/2;
+    n_eff_1 = (n_eff_1_prev + n_eff_1_post)/2;
 end
 
 
